ffa7a52796fe133e82242bf15ace107b768deaa2
thainabcc <thainabcc@pbtech.net.br>
2021-01-21 15:18:35 -0300

    feat: atualiza _app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 
-const App = () => {
-  return <div>App React</div>;
-};
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
 
 export default App;

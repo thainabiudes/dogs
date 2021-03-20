da12cdb1ad729410eaf20d8f57973f51551e0769
thainabcc <thainabcc@pbtech.net.br>
2021-03-19 23:10:24 -0300

    feat: cria componente footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,13 +2,7 @@ import React from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => {
-  return (
-    <div className={styles.footer}>
-      <h1>
-        Footer
-      </h1>
-    </div>
-  );
+  return <div className={styles.footer}>Footer</div>;
 };
 
 export default Footer;

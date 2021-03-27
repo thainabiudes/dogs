8d209577605f6b773aeba3ea46e3268ada9d476c
Thaina Biudes <thaina.biudes@gmail.com>
2021-03-27 19:16:52 -0300

    feat: cria proteção de rota

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './Components/Home';
-import Login from './Components/Login/Login';
-import { UserStorage } from './UserContext';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import User from "./Components/User/User";
+import { UserStorage } from "./UserContext";
+import ProtectedRoute from "./Helper/ProtectedRoute";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <UserStorage>
+        <UserStorage>
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <ProtectedRoute path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>

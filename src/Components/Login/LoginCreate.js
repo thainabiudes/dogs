ebe357f26921527cafcce34b9f227356cdfe4a0e
Thaina Biudes <thaina.biudes@gmail.com>
2021-03-26 21:33:18 -0300

    feat: implementa funconalidade de criar login

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
+import { USER_POST } from "../../api";
+import { UserContext } from "../../UserContext";
 
 const LoginCreate = () => {
-  return <div>Login Criar</div>;
+  const username = useForm();
+  const email = useForm("email");
+  const password = useForm("password");
+
+  const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+  }
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastra-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="username" {...email} />
+        <Input label="Password" type="password" name="username" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;

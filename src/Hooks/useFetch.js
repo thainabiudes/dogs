f646075683ff203608323bf6c37e8e79b838dce9
Thaina Biudes <thaina.biudes@gmail.com>
2021-03-27 19:19:46 -0300

    feat: cria hook useFetch

---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const useFetch = () => {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+
+  // useCallback para não ser recriado toda vezque formos utilizar o useFetch
+  const request = React.useCallback(async (url, options) => {
+    let response;
+    let json;
+    try {
+      setError(null);
+      setLoading(true);
+      response = await fetch(url, options);
+      json = await response.json();
+      if (response.ok === false) throw new Error();
+    } catch (err) {
+      json = null;
+      setError(err.message);
+    } finally {
+      setData(json);
+      setLoading(false);
+      return { response, json };
+    }
+  }, []);
+
+  return { data, error, loading, request };
+};
+
+export default useFetch;

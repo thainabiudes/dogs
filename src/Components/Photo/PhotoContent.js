85efaf2c6322f56aa81c0c6dc7309a383f9cac3b
Thaina Biudes <thaina.biudes@gmail.com>
2021-03-28 20:47:04 -0300

    feat: atualiza componentes Photo

---
+++ src/Components/Photo/PhotoContent.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./PhotoContent.module.css";
+import PhotoComments from "./PhotoComments";
+import { UserContext } from "../../UserContext";
+import PhotoDelete from "./PhotoDelete";
+import Image from "../../Helper/Image";
+
+const PhotoContent = ({ data, single }) => {
+  const { photo, comments } = data;
+  const user = React.useContext(UserContext);
+
+  return (
+    <div className={`${styles.photo} ${single ? styles.single : ""}`}>
+      <div className={styles.img}>
+        <Image src={photo.src} alt={photo.title} />
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p className={styles.author}>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
+            <span className={styles.view}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} kg</li>
+            <li>
+              {photo.idade} {photo.idade > 0 ? `anos` : ``}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <PhotoComments id={photo.id} comments={comments} />
+    </div>
+  );
+};
+
+export default PhotoContent;

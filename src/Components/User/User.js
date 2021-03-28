04ccf164d16896ab21c8c41769e103a024dc95c8
Thaina Biudes <thaina.biudes@gmail.com>
2021-03-28 20:47:23 -0300

    feat: atualiza componentes User

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -4,14 +4,21 @@ import UserHeader from "./UserHeader";
 import Feed from "../Feed/Feed";
 import UserPhotoPost from "./UserPhotoPost";
 import UserStats from "./UserStats";
+import { UserContext } from "../../UserContext";
+import NotFound from "../NotFound";
+import Head from "../../Helper/Head";
 
 const User = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <section className="container">
+      <Head title="Minha Conta" />
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="*" element={<NotFound />} />
         <Route path="estatisticas" element={<UserStats />} />
       </Routes>
     </section>
---
+++ src/Components/User/UserStats.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Head from "../../Helper/Head";
+import useFetch from "../../Hooks/useFetch";
+import { STATS_GET } from "../../api";
+import Error from "../../Helper/Error";
+import Loading from "../../Helper/Loading";
+
+const UserStatsGraphs = React.lazy(() => import("./UserStatsGraphs"));
+
+const UserStats = () => {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+
+    getData();
+  }, [request]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <React.Suspense fallback={<Loading />}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  else return null;
+};
+
+export default UserStats;

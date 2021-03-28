572975b25ef543a9675c2912eb778b1d496c4450
Thaina Biudes <thaina.biudes@gmail.com>
2021-03-28 20:48:12 -0300

    feat: consome componente Head

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Head from "../Helper/Head";
 import Feed from "./Feed/Feed";
 
 const Home = () => {
   return (
     <section className="container mainContainer">
+      <Head
+        title="Fotos"
+        description="Nome do site dogs, com o feed de fotos"
+      />
       <Feed />
     </section>
   );

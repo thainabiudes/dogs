55db59099800c685a87a20b6feb0191d1d7fb2e1
Thaina Biudes <thaina.biudes@gmail.com>
2021-03-28 20:40:51 -0300

    feat: consome novas rotas

--- src/App.js
+++ src/App.js
@@ -8,18 +8,26 @@ import Login from "./Components/Login/Login";
 import User from "./Components/User/User";
 import { UserStorage } from "./UserContext";
 import ProtectedRoute from "./Helper/ProtectedRoute";
+import Photo from "./Components/Photo/Photo";
+import UserProfile from "./Components/User/UserProfile";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="login/*" element={<Login />} />
-            <ProtectedRoute path="conta/*" element={<User />} />
-          </Routes>
+          <main className="AppBody">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <Route path="perfil/:user" element={<UserProfile />} />
+              <Route path="foto/:id" element={<Photo />} />
+              <Route path="*" element={<NotFound />} />
+              <ProtectedRoute path="conta/*" element={<User />} />
+            </Routes>
+          </main>
           <Footer />
         </UserStorage>
       </BrowserRouter>
